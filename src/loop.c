f3048835b7e871199821c35f23a386a6c6a3f985
Laetitia LEJEUNE <llejeune@e3r5p3.42.fr>
2019-03-18 09:32:06 +0100

    maj

--- src/loop.c
+++ src/loop.c
@@ -6,23 +6,11 @@
 /*   By: jleblond <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/15 14:27:53 by jleblond          #+#    #+#             */
-/*   Updated: 2019/03/15 18:48:37 by llejeune         ###   ########.fr       */
+/*   Updated: 2019/03/16 10:28:10 by llejeune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
-
-void		ft_print_menu(t_context *ct)
-{
-	ct->surface = SDL_LoadBMP("../H_BJPIC.bmp");
-	ct->surface == NULL ? exit(0) : 0;
-	ct->tmp = SDL_CreateTextureFromSurface(ct->rend, ct->surface);
-	SDL_FreeSurface(ct->surface);
-	SDL_SetRenderTarget(ct->rend, ct->texture);
-	SDL_RenderCopy(ct->rend, ct->tmp, NULL, NULL);
-	SDL_DestroyTexture(ct->tmp);
-	SDL_SetRenderTarget(ct->rend, NULL);
-}
 
 void		loop(t_context *ct)
 {
