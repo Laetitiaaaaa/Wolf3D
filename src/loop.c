3abd2496f2a50723556199e433b184cf7d72eb79
Jie LEBLOND <jleblond@e2r5p15.42.fr>
2019-03-19 15:54:05 +0100

    camera move done

--- src/loop.c
+++ src/loop.c
@@ -41,10 +41,14 @@ void	key_events(t_context *ct)
 
 	state = (Uint8*)SDL_GetKeyboardState(NULL);
 	state[SDL_SCANCODE_ESCAPE] ? exit(0) : 0;
-	state[SDL_SCANCODE_A] ? ct->cam.angle += 0.05 : 0;
-	state[SDL_SCANCODE_S] ? ct->cam.angle -= 0.05 : 0;
-	state[SDL_SCANCODE_UP] ? ct->cam.cam_position.y -= 0.005 : 0;
-	state[SDL_SCANCODE_DOWN] ? ct->cam.cam_position.y += 0.005 : 0;
-	state[SDL_SCANCODE_LEFT] ? ct->cam.cam_position.x -= 0.005 : 0;
-	state[SDL_SCANCODE_RIGHT] ? ct->cam.cam_position.x += 0.005 : 0;
+	state[SDL_SCANCODE_A] ? ct->cam.angle += 0.1 : 0;
+	state[SDL_SCANCODE_S] ? ct->cam.angle -= 0.1 : 0;
+	if (ct->cam.cam_position.y - 0.05 > 0)
+		state[SDL_SCANCODE_UP] ? ct->cam.cam_position.y -= 0.005 : 0;
+	if (ct->cam.cam_position.y + 0.05 < ct->mpp.y)
+		state[SDL_SCANCODE_DOWN] ? ct->cam.cam_position.y += 0.005 : 0;
+	if (ct->cam.cam_position.x - 0.05 > 0)
+		state[SDL_SCANCODE_LEFT] ? ct->cam.cam_position.x -= 0.005 : 0;
+	if (ct->cam.cam_position.x + 0.05 < ct->mpp.x)
+		state[SDL_SCANCODE_RIGHT] ? ct->cam.cam_position.x += 0.005 : 0;
 }
