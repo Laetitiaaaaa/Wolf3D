83f40041b2efa62bef25807b25be46bbd3deec72
touilleWoman <jie.zhou.ac@gmail.com>
2019-04-07 20:57:10 +0200

    draw icon done gst

--- srcs/draw_icon.c
+++ srcs/draw_icon.c
@@ -12,30 +12,25 @@
 
 #include "wolf3d.h"
 
-
-
-
 void	draw_text(t_context *ct, char *s, SDL_Point posi)
 {
 	SDL_Color		color = {165, 4, 13,  SDL_ALPHA_OPAQUE};
 	SDL_Surface		*surface;
 	SDL_Texture		*texture;
-	// TTF_Font		*font;
+	int				tex_width;
+	int				tex_height;
 
-	// font = TTF_OpenFont("/Library/Fonts/Arial.ttf", 35);
-	// if (font == NULL)
-		// quit("TTF_OpenFont()failed", ct);
 	surface = TTF_RenderText_Solid(ct->font, s, color);
 	if (surface == NULL)
 		quit("TTF_RenderText_Solid()failed", ct);
-	// TTF_CloseFont(font);
 	texture = SDL_CreateTextureFromSurface(ct->rend, surface);
 	if (texture == NULL)
 		quit("SDL_CreateTextureFromSurface()failed", ct);
-	int	texw = 0;
-	int texh = 0;
-	SDL_QueryTexture(texture, NULL, NULL, &texw, &texh);
-	SDL_Rect dst= {posi.x, posi.y, texw, texh };
+	SDL_FreeSurface(surface);
+	tex_width = 0;
+	tex_height = 0;
+	SDL_QueryTexture(texture, NULL, NULL, &tex_width, &tex_height);
+	SDL_Rect dst= {posi.x, posi.y, tex_width, tex_height };
 	SDL_RenderCopy(ct->rend, texture, NULL, &dst);
 }
 
@@ -44,12 +39,19 @@ void	draw_icon(t_context *ct)
 	SDL_Rect dst_mushroom;
 	SDL_Rect dst_key;
 	SDL_Point	posi;
+	char		*s;
 
 	posi.x = 80;
 	posi.y = 20;
-	draw_text(ct, ft_itoa(ct->sp.mushroom_nb), posi);
+	s = ft_itoa(ct->sp.mushroom_nb);
+	draw_text(ct, s, posi);
+	free(s);
+	s = NULL;
 	posi.y = 80;
-	draw_text(ct, ft_itoa(ct->sp.key_nb), posi);
+	s = ft_itoa(ct->sp.key_nb);
+	draw_text(ct, s, posi);
+	free(s);
+	s = NULL;
 	dst_mushroom = define_rect(10, 10, 50, 50);
 	SDL_RenderCopy(ct->rend, ct->tex.mushroom, NULL, &dst_mushroom);
 	dst_key = define_rect(10, 70, 50, 50);
